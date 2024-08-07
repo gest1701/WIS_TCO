37a55bbbcaee9a0184ca50b6aeea70c3ff9f6bdc
Rob van Gestel <45030706+gest1701@users.noreply.github.com>
2024-08-07 10:53:06 +0200

    Versie 0

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import time
+
+
+st.image('https://wattsinstore.nl/wp-content/uploads/2024/02/Logo-WattsInStore.svg',width=400,caption='WIS Logo')
+'Welkom bij Watts In Store, deze pagina wordt gebouwd.'
